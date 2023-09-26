412733cebcb73d757a7070f1cdbe65a949eb543d
Alex09190 <tuj09190@temple.edu>
2023-09-26 15:24:59 -0400

    Fixed from ImageView to TextView

--- app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/MainActivity.kt
@@ -2,6 +2,7 @@ package edu.temple.simplerecyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -10,17 +11,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        val numbers = arrayOf (0, 0,0,0,0,0,0,0,0,0,0,0,0,0,0)
-        //lateinit var recyclerView: RecyclerView
+        val numbers = arrayOf (1,2,3,4,5,6,7,8,9,10)
 
         // Step 1: Reference RecyclerView object
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
 
         //Step 2: Provide a LayoutManager
-        recyclerView.layoutManager
+        recyclerView.layoutManager = LinearLayoutManager(this)
 
         //Step 4: Provide a RecyclerView.Adapter
-        recyclerView.adapter = NumberDisplayAdapter (numbers)
+        recyclerView.adapter = NumberDisplayAdapter(numbers)
 
     }
 }
--- app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
+++ app/src/main/java/edu/temple/simplerecyclerview/NumberDisplayAdapter.kt
@@ -1,20 +1,22 @@
 package edu.temple.simplerecyclerview
 
+
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import android.widget.TextView
 
 // Step 3: Complete adapter implementation
 
 class NumberDisplayAdapter (private val numbers: Array<Int>) : RecyclerView.Adapter<NumberDisplayAdapter.NumberViewHolder>() {
 
-    class NumberViewHolder(val textView: ImageView) : RecyclerView.ViewHolder (textView) {}
+    class NumberViewHolder(val textView: TextView) : RecyclerView.ViewHolder (textView) {}
 
 
 
     //Step 3b: Complete function definitions for adapter
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
-        return NumberViewHolder (ImageView (parent.context).apply{
+        return NumberViewHolder (TextView (parent.context).apply{
                 layoutParams = ViewGroup.LayoutParams (100 , 100 ) }
         )
     }
@@ -24,7 +26,7 @@ class NumberDisplayAdapter (private val numbers: Array<Int>) : RecyclerView.Adap
     }
 
     override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
-        holder.textView.setImageResource(numbers[position])
+        holder.textView.setText(numbers[position].toString())
     }
 
 
